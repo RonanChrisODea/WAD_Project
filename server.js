4e0880e1f5e2758f606e8141ce0f9a498411c690
RonanChrisODea <ronancodea@outlook.com>
2024-05-16 08:52:10 +0100

    Node.js server initalised

---
+++ server.js
@@ -0,0 +1,30 @@
+// Import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+// const mysql = require('mysql2');
+
+// Create an Express application
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Parse JSOn and URL encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Serve static files from static dir
+app.use(express.static('static'));
+
+
+// Define your routes here
+
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+});
